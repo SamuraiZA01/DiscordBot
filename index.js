cf84a221102c97a05948bf652e3701838335f283
SamuraiZA01 <47923854+SamuraiZA01@users.noreply.github.com>
2019-02-25 14:49:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ bot.on('ready', () => {
 
 bot.registry.registerGroup('meme', 'Meme');
 bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + 'SamuraiZA01/DiscordBot/commands');
+bot.registry.registerCommandsIn(__dirname + 'SamuraiZA01/DiscordBot/tree/master/commands/');
 
 global.servers = {};
 
